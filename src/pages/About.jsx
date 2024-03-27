a29637eb9e347f62145e24f7f765b07ce3a4cb13
alozievictor <aloziev50@gmail.com>
2024-03-27 18:58:46 +0100

    pushed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import img1 from "../assets/vacation3.jpg";
-import img2 from "../assets/vacation3.jpg";
 import img3 from "../assets/travel1.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 import { Plane } from "lucide-react";
 import Footer from "../components/Footer";
-import CeoImg from "../assets/ceo.jpg";
+import CeoImg from "../assets/Faruq.jfif";
 import Abt from "../assets/ABT1.jpeg";
 
 const About = () => {
@@ -34,21 +32,24 @@ const About = () => {
       >
         <div class="flex w-full mb-10">
           <div class="w-full relative">
-            
             <h1 class="text-white text-center capitalize font-bold text-[35px] md:text-5xl py-2">
               About us
             </h1>
-            <p class="text-white text-center capitalize font-light text-2xl md:text-2xl py-2">Modern & Beautiful Travel Theme</p>
+            <p class="text-white text-center capitalize font-light text-2xl md:text-2xl py-2">
+              Modern & Beautiful Travel Theme
+            </p>
           </div>
         </div>
       </main>
 
       <div className="w-full">
-
-        <div className="mt-20 pb-16 md:px-0 px-3">
+        <div className="mt-20 pb-10 md:px-0 px-3">
           <div className="grid md:grid-cols-2 gap-10 md:w-[80%] mx-auto">
-            <div className="border group py-5 px-2 md:px-4 md:rounded-s-full rounded-t-full">
-              <img src={CeoImg} className="md:rounded-s-full rounded-t-full" />
+            <div className="border group py-5 px-2 md:px-4 md:rounded-s-full rounded-t-full w-full h-[80vh]">
+              <img
+                src={CeoImg}
+                className="md:rounded-s-full rounded-t-full w-full h-[75vh]"
+              />
             </div>
             <div className="py-5 px-2 md:px-4 flex justify-center items-center">
               <div className="py-2">
@@ -56,19 +57,22 @@ const About = () => {
                   ceo statement.
                 </p>
                 <p className="text-[15px] text-center my-2 md:my-4">
-                  I am committed to providing you with the highest quality
-                  travel experiences, tailored to your interests and
-                  preferences. Whether you're seeking adventure in the great
-                  outdoors, cultural immersion in bustling cities, or relaxation
-                  on pristine beaches, we have the perfect itinerary for you.
+                  Our team of experienced travel experts works tirelessly to
+                  curate unforgettable journeys that cater to your unique
+                  desires and passions. We understand that every traveler is
+                  different, which is why we take the time to listen to your
+                  needs and craft personalized itineraries that exceed your
+                  expectations.
                 </p>
                 <p className="text-center text-xl font-semibold uppercase">
                   Mission.
                 </p>
                 <p className="text-[15px] text-center my-2 md:my-4">
-                  our mission is to inspire and empower people to explore the
-                  world and enrich their lives through transformative travel
-                  experiences.
+                  our mission is to create memorable and meaningful travel
+                  experiences that go beyond the typical tourist attractions,
+                  allowing our clients to immerse themselves in different
+                  cultures, connect with local communities, and create lifelong
+                  memories.
                 </p>
               </div>
             </div>
@@ -95,19 +99,19 @@ const About = () => {
                 </h2>
                 <div className="md:w-[80%] md:mb-8">
                   <p className="text-[16px] text-justify text-gray-800 ">
-                    At Lucidreams, we're committed to making your international
+                    At Travelo, we're committed to making your international
                     journey smooth and rewarding. We prioritize safety, cultural
                     sensitivity, and sustainability in all our endeavors,
                     ensuring that your experiences abroad are enriching and
                     memorable.
                   </p>
                 </div>
-                <button
+                {/* <button
                   type="button"
                   className="p-4 w-60 bg-[#265670] hover:bg-[#4C7890] cursor-pointer text-white lg:w-40 rounded-md mt-8"
                 >
                   Subcribe
-                </button>
+                </button> */}
               </div>
             </div>
             <div class="box-img py-2">
